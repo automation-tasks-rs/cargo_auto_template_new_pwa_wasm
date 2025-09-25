f647342c3650ba20bf95e29c2ae8037347418a63
bestia.dev <info@bestia.dev>
2025-09-25 16:23:56 +0000

    update docs

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["cargo_auto_template_new_pwa_wasm",["",[["main_mod",[["lib_mod",[["web_sys_mod",[],["html_source_code_mod.rs"]]],["hello_mod.rs","web_sys_mod.rs"]]],["lib_mod.rs"]]],["lib.rs","main_mod.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[193]}
+createSrcSidebar('[["cargo_auto_template_new_pwa_wasm",["",[["main_mod",[["lib_mod",[["web_sys_mod",[],["html_source_code_mod.rs"]]],["hello_mod.rs","web_sys_mod.rs"]]],["lib_mod.rs"]]],["lib.rs","main_mod.rs"]]]]');
+//{"start":19,"fragment_lengths":[193]}
